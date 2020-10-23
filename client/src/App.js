18f8fd69b4f8f016ea798183d7bc27b61b8f27b7
Melissa Day <melissaday076@gmail.com>
2020-10-23 18:30:54 -0400

    react added and initial setup complete

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import React, { Fragment } from 'react';
+import './App.css';
+import Layout from './components/layout/index';
+
+const App = () => 
+  
+    <Fragment>
+      <Layout />
+    </Fragment>
+ 
+
+
+export default App;
